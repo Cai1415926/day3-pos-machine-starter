cd2fc8d3dda5b5b19ea8301d3cda6ccf76301060
VICTOR <victor.cai@oocl.com>
2025-09-03 18:57:41 +0800

    feat: calculate item cost

---
+++ src/main/java/pos/machine/Receipt.java
@@ -0,0 +1,13 @@
+package pos.machine;
+
+import java.util.List;
+
+public class Receipt {
+    private final List<ReceiptItem> receiptItems;
+    private final int totalPrice;
+
+    public Receipt(List<ReceiptItem> receiptItems, int totalPrice) {
+        this.receiptItems = receiptItems;
+        this.totalPrice = totalPrice;
+    }
+}
