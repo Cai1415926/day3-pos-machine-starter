3dd9c524ec8d372c0931416d07133b25c201af36
VICTOR <victor.cai@oocl.com>
2025-09-03 18:39:29 +0800

    feat: decode Item to ReceiptItem

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -6,4 +6,19 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
+
+    public List<ReceiptItem> decodeToItems(List<String> barcodes) {
+        List<Item> items = loadAllItems();
+        return items.stream().map(item -> {
+            int count = (int) barcodes.stream().filter(barcode -> barcode.equals(item.getBarcode())).count();
+            if (count == 0) {
+                throw new RuntimeException("Item with barcode " + item.getBarcode() + " not found in the input barcodes.");
+            }
+            return new ReceiptItem(item.getBarcode(), item.getName(), item.getPrice(), item.getPrice() * count);
+        }).toList();
+    }
+
+    public List<Item> loadAllItems() {
+        return ItemsLoader.loadAllItems();
+    }
 }
---
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -0,0 +1,14 @@
+package pos.machine;
+
+public class ReceiptItem extends Item{
+    private final int subTotal;
+
+    public ReceiptItem(String barcode, String name, int price, int subTotal) {
+        super(barcode, name, price);
+        this.subTotal = subTotal;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+}
