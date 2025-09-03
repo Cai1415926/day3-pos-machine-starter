94a5a3c795c98983bf685968847d79c13d8b6a05
VICTOR <victor.cai@oocl.com>
2025-09-03 18:57:11 +0800

    feat: calculate item cost

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -14,11 +14,25 @@ public class PosMachine {
             if (count == 0) {
                 throw new RuntimeException("Item with barcode " + item.getBarcode() + " not found in the input barcodes.");
             }
-            return new ReceiptItem(item.getBarcode(), item.getName(), item.getPrice(), item.getPrice() * count);
+            return new ReceiptItem(item.getBarcode(), item.getName(), item.getPrice());
         }).toList();
     }
 
     public List<Item> loadAllItems() {
         return ItemsLoader.loadAllItems();
+    }
+
+    public Receipt calculateCost(List<ReceiptItem> receiptItems) {
+        List<ReceiptItem> receiptItems1 = calculateItemsCost(receiptItems);
+
+        return null;
+    }
+
+    public List<ReceiptItem> calculateItemsCost(List<ReceiptItem> receiptItems) {
+        return receiptItems.stream().map(receiptItem -> {
+            int count = (int) receiptItems.stream()
+                    .filter(tmp -> tmp.getBarcode().equals(receiptItem.getBarcode())).count();
+            return new ReceiptItem(receiptItem.getBarcode(), receiptItem.getName(), receiptItem.getPrice(), receiptItem.getPrice() * count);
+        }).toList();
     }
 }
--- src/main/java/pos/machine/ReceiptItem.java
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -3,6 +3,11 @@ package pos.machine;
 public class ReceiptItem extends Item{
     private final int subTotal;
 
+    public ReceiptItem(String barcode, String name, int price) {
+        super(barcode, name, price);
+        this.subTotal = 0;
+    }
+
     public ReceiptItem(String barcode, String name, int price, int subTotal) {
         super(barcode, name, price);
         this.subTotal = subTotal;
